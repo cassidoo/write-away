6c64b861304ec6903586dca70da1cf9a5d1753b4
Cassidy Williams <cassidoo@outlook.com>
2014-05-04 21:00:52 -0500

    Added UI JS template and font

---
+++ js/ui.js
@@ -0,0 +1,50 @@
+function init()
+{
+    console.log("Fiddling under the hood, eh buddy? We've probably got a lot in common. You should check out my GitHub (https://github.com/cassidoo)!");
+}
+
+/*
+ Editing text stuff
+ */
+
+function editDialog()
+{
+    
+}
+
+function formatText(type)
+{
+    
+}
+
+/*
+ Local Storage stuff... I need to learn how to do this.
+ */
+
+function loadState()
+{
+    
+}
+
+function saveState() // Maybe also save to Dropbox?
+{
+    
+}
+
+/*
+ Full Screen stuff... I should also learn how to do this.
+ */
+
+function enterFullScreen()
+{
+    
+}
+
+/*
+ Word count stuff
+ */
+
+function wordCount()
+{
+    
+}
