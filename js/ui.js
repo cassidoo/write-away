17fe2033d0a539dc9800b2855254bb7b24ac6fb2
Cassidy Williams <cassidoo@outlook.com>
2014-05-14 23:08:14 -0500

    Countdown clock!
    IT WORKS WEEEEE

--- js/ui.js
+++ js/ui.js
@@ -1,9 +1,77 @@
 var bg = "normal";
 // set from local storage later
 
+console.log("Fiddling under the hood, eh buddy? We've probably got a lot in common. You should check out my GitHub (https://github.com/cassidoo)!");
+
 function init()
 {
-    console.log("Fiddling under the hood, eh buddy? We've probably got a lot in common. You should check out my GitHub (https://github.com/cassidoo)!");
+    var minsnum = document.getElementById("minsnum").value;
+    
+    if(minsnum <= 0)
+    {
+        console.log("Psh gurl whatchu doin");
+    }
+
+    var countdown = new Countdown(
+    {
+        seconds : minsnum * 60,
+        onUpdateStatus : function(sec)
+        {
+            var minute = Math.floor(sec / 60);
+            var second = (sec % 60);
+            if(second < 10)
+            {
+                second = "0" + second;
+            }
+            document.getElementById("timer").innerHTML = minute + ":" + second;
+        },
+        onCounterEnd : function()
+        {
+            alert('Document ended!');
+        }
+    });
+
+    countdown.start();
+}
+
+function Countdown(options)
+{
+    var timer;
+    var instance = this;
+    var seconds = options.seconds || 10;
+    var updateStatus = options.onUpdateStatus ||
+    function()
+    {
+    };
+    var counterEnd = options.onCounterEnd ||
+    function()
+    {
+    };
+
+    function decrementCounter()
+    {
+        updateStatus(seconds);
+        if(seconds === 0)
+        {
+            counterEnd();
+            instance.stop();
+        }
+        seconds--;
+    }
+
+
+    this.start = function()
+    {
+        clearInterval(timer);
+        timer = 0;
+        seconds = options.seconds;
+        timer = setInterval(decrementCounter, 1000);
+    };
+
+    this.stop = function()
+    {
+        clearInterval(timer);
+    };
 }
 
 function toggleColors()
